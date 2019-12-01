dfe9ebb916add10834b4a0e35402ef470aabd8dc
HUANGZHUO <huangzhuo@symdata.cn>
2019-12-01 18:11:20 +0800

    代码优化

--- src/main/java/com/hz/smsgate/base/constants/SmppServerConstants.java
+++ src/main/java/com/hz/smsgate/base/constants/SmppServerConstants.java
@@ -6,70 +6,84 @@ package com.hz.smsgate.base.constants;
  * @Description:
  */
 public class SmppServerConstants {
-	/**
-	 * CM msgid的缓存key
-	 */
-	public static final String CM_MSGID_CACHE = "cmMsgIdCache";
-	/**
-	 * CM 短信下行缓存key
-	 */
-	public static final String CM_SUBMIT_SM = "cmSubmitSm";
+    /**
+     * CM msgid的缓存key
+     */
+    public static final String CM_MSGID_CACHE = "cmMsgIdCache";
+    /**
+     * CM 短信下行缓存key
+     */
+    public static final String CM_SUBMIT_SM = "cmSubmitSm";
 
-	/**
-	 *CM 状态报告缓存key
-	 */
-	public static final String CM_DELIVER_SM = "cmDeliverSm";
-
-
-	/**
-	 * WEB msgid的缓存key
-	 */
-	public static final String WEB_MSGID_CACHE = "webMsgIdCache";
+    /**
+     * CM 状态报告缓存key
+     */
+    public static final String CM_DELIVER_SM = "cmDeliverSm";
 
 
-	/**
-	 * WEB 短短信下行 opt
-	 */
-	public static final String WEB_SUBMIT_SM_OPT = "webSubmitSmOpt";
-	/**
-	 * WEB 短短信下行 tz
-	 */
-	public static final String WEB_SUBMIT_SM_TZ = "webSubmitSmTz";
-	/**
-	 * WEB 短短信下行 yx
-	 */
-	public static final String WEB_SUBMIT_SM_YX = "webSubmitSmYx";
-
-	/**
-	 * WEB 长短信下行缓存key
-	 */
-	public static final String WEB_LONG_SUBMIT_SM = "webLongSubmitSm";
-
-	/**
-	 * WEB 长短信合并
-	 */
-	public static final String WEB_LONG_SUBMIT_SM_SEND = "webLongSubmitSmSend";
-
-	/**
-	 * WEB 长短信真实发送
-	 */
-	public static final String WEB_REL_LONG_SUBMIT_SM_SEND = "webRealLongSubmitSmSend";
+    /**
+     * WEB msgid的缓存key
+     */
+    public static final String WEB_MSGID_CACHE = "webMsgIdCache";
 
 
-	/**
-	 * 同步请求到网关
-	 */
-	public static final String SYNC_SUBMIT = "syncSubmit";
+    /**
+     * WEB 短短信下行 opt
+     */
+    public static final String WEB_SUBMIT_SM_OPT = "webSubmitSmOpt";
+    /**
+     * WEB 短短信下行 tz
+     */
+    public static final String WEB_SUBMIT_SM_TZ = "webSubmitSmTz";
+    /**
+     * WEB 短短信下行 yx
+     */
+    public static final String WEB_SUBMIT_SM_YX = "webSubmitSmYx";
 
+    /**
+     * WEB 长短信下行缓存key
+     */
+    public static final String WEB_LONG_SUBMIT_SM = "webLongSubmitSm";
+
+    /**
+     * WEB 长短信下行缓存  opt
+     */
+    public static final String WEB_LONG_SUBMIT_SM_OPT = "webLongSubmitSmOpt";
+
+    /**
+     * WEB 长短信下行缓存  通知
+     */
+    public static final String WEB_LONG_SUBMIT_SM_TZ = "webLongSubmitSmTz";
+
+    /**
+     * WEB 长短信下行缓存  营销
+     */
+    public static final String WEB_LONG_SUBMIT_SM_YX = "webLongSubmitSmYx";
 
 
 
 
+    /**
+     * WEB 长短信合并
+     */
+    public static final String WEB_LONG_SUBMIT_SM_SEND = "webLongSubmitSmSend";
 
-	/**
-	 *WEB 状态报告缓存key
-	 */
-	public static final String WEB_DELIVER_SM = "webDeliverSm";
+    /**
+     * WEB 长短信真实发送
+     */
+    public static final String WEB_REL_LONG_SUBMIT_SM_SEND = "webRealLongSubmitSmSend";
+
+
+    /**
+     * 同步请求到网关
+     */
+    public static final String SYNC_SUBMIT = "syncSubmit";
+
+
+    /**
+     * WEB 状态报告缓存key
+     */
+    public static final String WEB_DELIVER_SM = "webDeliverSm";
 
 
 }
