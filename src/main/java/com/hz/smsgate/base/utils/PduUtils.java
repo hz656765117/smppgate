ce4b9dea210438ca70d54f90fcc0a2e16dd3c8c6
huangzhuo <656765117@qq.com>
2019-12-04 01:13:57 +0800

    msgid缓存优化，心跳和获取session优化

--- src/main/java/com/hz/smsgate/base/utils/PduUtils.java
+++ src/main/java/com/hz/smsgate/base/utils/PduUtils.java
@@ -26,57 +26,57 @@ import java.util.Set;
  * @Description:
  */
 public class PduUtils {
-	private static Logger LOGGER = LoggerFactory.getLogger(PduUtils.class);
+    private static Logger LOGGER = LoggerFactory.getLogger(PduUtils.class);
 
-	/**
-	 * 通道555，778,779的短信去掉前面两个00
-	 *
-	 * @param sm
-	 * @return
-	 */
-	public static SubmitSm removeZero(SubmitSm sm) {
-		String channel = sm.getSourceAddress().getAddress();
-		if (channel.equals(StaticValue.CHANNL_REL.get(StaticValue.CHANNEL_1).getSenderId()) || channel.equals(StaticValue.CHANNEL_1) || StaticValue.CHANNEL_MK_LIST.contains(channel)) {
-			Address destAddress = sm.getDestAddress();
-			if (destAddress.getAddress().startsWith("00")) {
-				String address = destAddress.getAddress().substring(2);
-				destAddress.setAddress(address);
-				sm.setDestAddress(destAddress);
-				sm.calculateAndSetCommandLength();
-			}
-		}
-		return sm;
-	}
-
-
-	/**
-	 * 获取区号
-	 *
-	 * @param mbl
-	 * @return
-	 */
-	public static String getAreaCode(String mbl) {
-		String areaCode = "";
-		if (StringUtils.isBlank(mbl)) {
-			return areaCode;
-		}
-
-		if (mbl.startsWith("00")) {
-			areaCode = mbl.substring(2, 4);
-		} else {
-			areaCode = mbl.substring(0, 2);
-		}
-
-		return areaCode;
-	}
+    /**
+     * 通道555，778,779的短信去掉前面两个00
+     *
+     * @param sm
+     * @return
+     */
+    public static SubmitSm removeZero(SubmitSm sm) {
+        String channel = sm.getSourceAddress().getAddress();
+        if (channel.equals(StaticValue.CHANNL_REL.get(StaticValue.CHANNEL_1).getSenderId()) || channel.equals(StaticValue.CHANNEL_1) || StaticValue.CHANNEL_MK_LIST.contains(channel)) {
+            Address destAddress = sm.getDestAddress();
+            if (destAddress.getAddress().startsWith("00")) {
+                String address = destAddress.getAddress().substring(2);
+                destAddress.setAddress(address);
+                sm.setDestAddress(destAddress);
+                sm.calculateAndSetCommandLength();
+            }
+        }
+        return sm;
+    }
 
 
-	/**
-	 * 根据通道获取用户名
-	 *
-	 * @param sendId 通道id
-	 * @return
-	 */
+    /**
+     * 获取区号
+     *
+     * @param mbl
+     * @return
+     */
+    public static String getAreaCode(String mbl) {
+        String areaCode = "";
+        if (StringUtils.isBlank(mbl)) {
+            return areaCode;
+        }
+
+        if (mbl.startsWith("00")) {
+            areaCode = mbl.substring(2, 4);
+        } else {
+            areaCode = mbl.substring(0, 2);
+        }
+
+        return areaCode;
+    }
+
+
+    /**
+     * 根据通道获取用户名
+     *
+     * @param sendId 通道id
+     * @return
+     */
 //	public static String getSystemIdBySendId(String sendId) {
 //		SmppSessionConfiguration smppSessionConfiguration = ClientInit.configMap.get(sendId);
 //		if (smppSessionConfiguration == null) {
@@ -91,178 +91,165 @@ public class PduUtils {
 //	}
 
 
-	/**
-	 * 短信内容GSM编码  cm运营商的需要编码
-	 *
-	 * @param sm 下行短信对象
-	 * @return
-	 */
-	public static SubmitSm encodeGsm(SubmitSm sm) {
-		String systemId = sm.getSystemId();
-		//cm资源需要GSM格式编码
-		if (StaticValue.SYSTEMID_CM_1.equals(systemId) || StaticValue.SYSTEMID_CM_2.equals(systemId) || StaticValue.SYSTEMID_ALEX.equals(systemId)) {
-			onlyEncodeGsm(sm);
-		}
-		return sm;
-	}
+    /**
+     * 短信内容GSM编码  cm运营商的需要编码
+     *
+     * @param sm 下行短信对象
+     * @return
+     */
+    public static SubmitSm encodeGsm(SubmitSm sm) {
+        String systemId = sm.getSystemId();
+        //cm资源需要GSM格式编码
+        if (StaticValue.SYSTEMID_CM_1.equals(systemId) || StaticValue.SYSTEMID_CM_2.equals(systemId) || StaticValue.SYSTEMID_ALEX.equals(systemId)) {
+            onlyEncodeGsm(sm);
+        }
+        return sm;
+    }
 
-	/**
-	 * 短信内容Gsm编码
-	 *
-	 * @param sm 下行短信对象
-	 * @return
-	 */
-	public static SubmitSm onlyEncodeGsm(SubmitSm sm) {
-		byte[] shortMessage = sm.getShortMessage();
-		String content = new String(shortMessage);
-		LOGGER.info("短短信的内容为{},下行号码为{}，通道为{}", content, sm.getDestAddress().getAddress(), sm.getSourceAddress().getAddress());
-		try {
-			byte[] textBytes = CharsetUtil.encode(content, CharsetUtil.CHARSET_GSM);
-			sm.setShortMessage(textBytes);
-		} catch (Exception e) {
-			LOGGER.error("短信内容编码异常", e);
-		}
-		LOGGER.info("短短信编码后的内容为{},下行号码为{}，通道为{}", new String(content.getBytes()), sm.getDestAddress().getAddress(), sm.getSourceAddress().getAddress());
+    /**
+     * 短信内容Gsm编码
+     *
+     * @param sm 下行短信对象
+     * @return
+     */
+    public static SubmitSm onlyEncodeGsm(SubmitSm sm) {
+        byte[] shortMessage = sm.getShortMessage();
+        String content = new String(shortMessage);
+        LOGGER.info("短短信的内容为{},下行号码为{}，通道为{}", content, sm.getDestAddress().getAddress(), sm.getSourceAddress().getAddress());
+        try {
+            byte[] textBytes = CharsetUtil.encode(content, CharsetUtil.CHARSET_GSM);
+            sm.setShortMessage(textBytes);
+        } catch (Exception e) {
+            LOGGER.error("短信内容编码异常", e);
+        }
+        LOGGER.info("短短信编码后的内容为{},下行号码为{}，通道为{}", new String(content.getBytes()), sm.getDestAddress().getAddress(), sm.getSourceAddress().getAddress());
 
-		sm.calculateAndSetCommandLength();
-		return sm;
-	}
-
-
-	/**
-	 * 重写下行对象
-	 *
-	 * @param sm
-	 * @return
-	 */
-	public static SubmitSm rewriteSubmitSm(SubmitSm sm) {
-
-		//通道替换
-		sm = PduUtil.rewriteSmSourceAddress(sm);
-
-		//短信下行内容编码
-		sm = PduUtils.encodeGsm(sm);
-		//通道555的短信去掉前面两个00
-		sm = PduUtils.removeZero(sm);
-		return sm;
-	}
+        sm.calculateAndSetCommandLength();
+        return sm;
+    }
 
 
-	/**
-	 * 根据下行通道获取对应的客户端session  TODO  暂时不支持多个客户端获取
-	 *
-	 * @param sm
-	 * @return
-	 */
-	public static SmppSession getSmppSession(SubmitSm sm) {
-		String sendId = sm.getSourceAddress().getAddress();
-		String systemId = sm.getSystemId();
+    /**
+     * 重写下行对象
+     *
+     * @param sm
+     * @return
+     */
+    public static SubmitSm rewriteSubmitSm(SubmitSm sm) {
+
+        //通道替换
+        sm = PduUtil.rewriteSmSourceAddress(sm);
+
+        //短信下行内容编码
+        sm = PduUtils.encodeGsm(sm);
+        //通道555的短信去掉前面两个00
+        sm = PduUtils.removeZero(sm);
+        return sm;
+    }
 
 
-		String key = getKey(systemId, sendId);
-
-		SessionKey sessionKey = new SessionKey();
-		sessionKey.setSystemId(systemId);
-		sessionKey.setSenderId(sendId);
-
-
-		SmppSession session0 = null;
-		Map<SessionKey, SmppSession> sessionMap = ClientInit.sessionMap;
-		if (sessionMap != null && sessionMap.size() > 0) {
-			session0 = sessionMap.get(sessionKey);
-			if (session0 == null) {
-				sessionKey.setSenderId(key);
-				session0 = sessionMap.get(sessionKey);
-			}
-		}
-
-		if (session0 == null) {
-			try {
-				sessionKey.setSenderId(sendId);
-				SmppSessionConfiguration smppSessionConfiguration = ClientInit.configMap.get(sessionKey);
-				if (smppSessionConfiguration == null) {
-					sessionKey.setSenderId(key);
-					smppSessionConfiguration = ClientInit.configMap.get(sessionKey);
-				}
-
-				session0 = ClientInit.createClient(smppSessionConfiguration);
-				if (session0 == null) {
-					sessionMap.remove(sessionKey);
-				} else {
-					return session0;
-				}
-
-				DefaultSmppClient defaultSmppClient = ClientInit.clientBootstrapMap.get(systemId);
-				if (defaultSmppClient == null) {
-					LOGGER.error("systemid({})获取defaultSmppClient异常", systemId);
-				}
-
-				DefaultSmppSessionHandler defaultSmppSessionHandler = ClientInit.sessionHandlerMap.get(systemId);
-				if (defaultSmppSessionHandler == null) {
-					LOGGER.error("systemid({})获取defaultSmppSessionHandler异常", systemId);
-				}
-
-				session0 = defaultSmppClient.bind(smppSessionConfiguration, defaultSmppSessionHandler);
-				ClientInit.sessionMap.put(sessionKey, session0);
-			} catch (Exception e) {
-				LOGGER.error("获取客户端连接异常", e);
-			}
-		}
-		return session0;
-	}
-
-	public static String getKey(String systemId, String sendId) {
-		Map<String, SessionKey> channlRel = StaticValue.CHANNL_REL;
-		SessionKey sessionKey = new SessionKey(systemId, sendId);
-		for (Map.Entry<String, SessionKey> entry : channlRel.entrySet()) {
-			if (sessionKey.equals(entry.getValue())) {
-				sendId = entry.getKey();
-				break;
-			}
-		}
-		return sendId;
-	}
+    /**
+     * 根据下行通道获取对应的客户端session  TODO  暂时不支持多个客户端获取
+     *
+     * @param sm
+     * @return
+     */
+    public static SmppSession getSmppSession(SubmitSm sm) {
+        String sendId = sm.getSourceAddress().getAddress();
+        String systemId = sm.getSystemId();
 
 
-	//获取原通道
-	public static String getRealChannel(String systemId, String gwChannel) {
-		if (StringUtils.isBlank(gwChannel)) {
-			return gwChannel;
-		}
-		SessionKey sessionKey = new SessionKey(systemId, gwChannel);
-		for (Map.Entry<String, SessionKey> entry : StaticValue.CHANNL_REL.entrySet()) {
-			if (sessionKey.equals(entry.getValue())) {
-				return entry.getKey();
-			}
-		}
-		return gwChannel;
-	}
+        String key = getKey(systemId, sendId);
+
+        SessionKey sessionKey = new SessionKey();
+        sessionKey.setSystemId(systemId);
+        sessionKey.setSenderId(sendId);
 
 
-	public static SmppSession getServerSmppSession(DeliverSm deliverSm) {
-		SmppSession smppSession = null;
-		//根据通道获取session
-		String channel = deliverSm.getDestAddress().getAddress();
-		String systemId = deliverSm.getSystemId();
+        SmppSession session0 = null;
+        Map<SessionKey, SmppSession> sessionMap = ClientInit.sessionMap;
+        if (sessionMap != null && sessionMap.size() > 0) {
+            session0 = sessionMap.get(sessionKey);
+            if (session0 == null) {
+                sessionKey.setSenderId(key);
+                session0 = sessionMap.get(sessionKey);
+            }
+        }
+
+        if (session0 == null || !session0.isBound()) {
+            try {
+                sessionKey.setSenderId(sendId);
+                SmppSessionConfiguration smppSessionConfiguration = ClientInit.configMap.get(sessionKey);
+                if (smppSessionConfiguration == null) {
+                    sessionKey.setSenderId(key);
+                    smppSessionConfiguration = ClientInit.configMap.get(sessionKey);
+                }
+
+                session0 = ClientInit.createClient(smppSessionConfiguration);
+                if (session0 != null) {
+                    return session0;
+                }
+            } catch (Exception e) {
+                LOGGER.error("获取客户端连接异常", e);
+            }
+        }
 
 
-		if (DefaultSmppServer.smppSessionList == null || DefaultSmppServer.smppSessionList.size() < 1) {
-			LOGGER.error("{}-处理状态报告异常，未能获取到服务端连接(通道为：{}，systemId为：{})-------", Thread.currentThread().getName(), channel, systemId);
-			return smppSession;
-		}
+        return session0;
+    }
 
-		for (SmppSession session : DefaultSmppServer.smppSessionList) {
-			if (session.getConfiguration().getSystemId().equals(systemId)) {
-				smppSession = session;
-				break;
-			}
-		}
+    public static String getKey(String systemId, String sendId) {
+        Map<String, SessionKey> channlRel = StaticValue.CHANNL_REL;
+        SessionKey sessionKey = new SessionKey(systemId, sendId);
+        for (Map.Entry<String, SessionKey> entry : channlRel.entrySet()) {
+            if (sessionKey.equals(entry.getValue())) {
+                sendId = entry.getKey();
+                break;
+            }
+        }
+        return sendId;
+    }
 
-		if (smppSession == null) {
-			LOGGER.error("{}-处理状态报告异常，未能匹配到服务端连接(通道为：{}，systemId为：{})-------", Thread.currentThread().getName(), channel, systemId);
-			return smppSession;
-		}
-		return smppSession;
-	}
+
+    //获取原通道
+    public static String getRealChannel(String systemId, String gwChannel) {
+        if (StringUtils.isBlank(gwChannel)) {
+            return gwChannel;
+        }
+        SessionKey sessionKey = new SessionKey(systemId, gwChannel);
+        for (Map.Entry<String, SessionKey> entry : StaticValue.CHANNL_REL.entrySet()) {
+            if (sessionKey.equals(entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return gwChannel;
+    }
+
+
+    public static SmppSession getServerSmppSession(DeliverSm deliverSm) {
+        SmppSession smppSession = null;
+        //根据通道获取session
+        String channel = deliverSm.getDestAddress().getAddress();
+        String systemId = deliverSm.getSystemId();
+
+
+        if (DefaultSmppServer.smppSessionList == null || DefaultSmppServer.smppSessionList.size() < 1) {
+            LOGGER.error("{}-处理状态报告异常，未能获取到服务端连接(通道为：{}，systemId为：{})-------", Thread.currentThread().getName(), channel, systemId);
+            return smppSession;
+        }
+
+        for (SmppSession session : DefaultSmppServer.smppSessionList) {
+            if (session.getConfiguration().getSystemId().equals(systemId)) {
+                smppSession = session;
+                break;
+            }
+        }
+
+        if (smppSession == null) {
+            LOGGER.error("{}-处理状态报告异常，未能匹配到服务端连接(通道为：{}，systemId为：{})-------", Thread.currentThread().getName(), channel, systemId);
+            return smppSession;
+        }
+        return smppSession;
+    }
 
 }
--- src/main/java/com/hz/smsgate/business/smpp/handler/Client1SmppSessionHandler.java
+++ src/main/java/com/hz/smsgate/business/smpp/handler/Client1SmppSessionHandler.java
@@ -49,175 +49,177 @@ import java.util.concurrent.BlockingQueue;
 @Component
 public class Client1SmppSessionHandler extends DefaultSmppSessionHandler {
 
-	private WeakReference<SmppSession> sessionRef;
+    private WeakReference<SmppSession> sessionRef;
 
-	@Autowired
-	public RedisUtil redisUtil;
+    @Autowired
+    public RedisUtil redisUtil;
 
-	public static Client1SmppSessionHandler client1SmppSessionHandler;
+    public static Client1SmppSessionHandler client1SmppSessionHandler;
 
-	@PostConstruct
-	public void init() {
-		client1SmppSessionHandler = this;
-		client1SmppSessionHandler.redisUtil = this.redisUtil;
-	}
-
-
-	@Override
-	public void setSmppSession(SmppSession session) {
-		this.sessionRef = new WeakReference<>(session);
-	}
-
-	private static final Logger logger = LoggerFactory.getLogger(Client1SmppSessionHandler.class);
-
-	public Client1SmppSessionHandler() {
-		super();
-	}
-
-	public Client1SmppSessionHandler(Logger logger) {
-		super(logger);
-	}
-
-	@Override
-	public String lookupResultMessage(int commandStatus) {
-		return super.lookupResultMessage(commandStatus);
-	}
-
-	@Override
-	public String lookupTlvTagName(short tag) {
-		return super.lookupTlvTagName(tag);
-	}
-
-	@Override
-	public void fireChannelUnexpectedlyClosed() {
-		super.fireChannelUnexpectedlyClosed();
-	}
-
-	@Override
-	public PduResponse firePduRequestReceived(PduRequest pduRequest) {
-		PduResponse response = pduRequest.createResponse();
-		SmppSession session = this.sessionRef.get();
-		String systemId = "";
-		if (session != null) {
-			systemId = session.getConfiguration().getSystemId();
-		}
-
-		try {
-			if (pduRequest.isRequest()) {
-				switch (pduRequest.getCommandId()) {
-					case SmppConstants.CMD_ID_SUBMIT_SM:
-						logger.error("----------客户端怎么会接到短信请求呢 ---------------");
-						break;
-					case SmppConstants.CMD_ID_DELIVER_SM:
-						DeliverSm deliverSm = (DeliverSm) pduRequest;
-
-						deliverSm.setSystemId(systemId);
-
-						try {
-							//0 je   1 redis
-							if ("1".equals(StaticValue.TYPE)) {
-								putRedisCache(deliverSm);
-							} else {
-								BlockingQueue<Object> queue = BDBStoredMapFactoryImpl.INS.getQueue("rptrvok", "rptrvok");
-								queue.put(deliverSm);
-							}
-
-						} catch (Exception e) {
-							logger.error("----------状态报告接收，加入队列异常。---------------", e);
-						}
-
-						break;
-					case SmppConstants.CMD_ID_DATA_SM:
-						return response;
-					case SmppConstants.CMD_ID_ENQUIRE_LINK:
-						logger.info("---------客户端{}也会接收心跳请求----------{}", systemId, pduRequest);
-						return response;
-					case SmppConstants.CMD_ID_UNBIND:
-						logger.info("---------客户端{}被解绑了----------{}", systemId, pduRequest);
-						return response;
-					default:
-						logger.error("----------客户端{}接收 未知异常。---------------{}", systemId, pduRequest);
-						return response;
-
-				}
-			}
-
-		} catch (Exception e) {
-			logger.error("--------客户端处理异常---------", e);
-		}
-
-		// do any logic here
-
-		return response;
-	}
-
-	/**
-	 * 将状态报告缓存到对应的redis中，交由不同的服务端处理
-	 *
-	 * @param deliverSm
-	 */
-	public static void putRedisCache(DeliverSm deliverSm) {
-		try {
-			Object obj = client1SmppSessionHandler.redisUtil.hmGetAllValues(SmppServerConstants.CM_MSGID_CACHE);
-			List<String> realMsgIds = (List<String>) obj;
-
-			//获取状态报告msgid
-			String str = new String(deliverSm.getShortMessage());
-			DeliveryReceipt deliveryReceipt = DeliveryReceipt.parseShortMessage(str, DateTimeZone.UTC);
-			String messageId = deliveryReceipt.getMessageId();
-
-			//判断msgid是否在cm的msgid缓存中存在
-			if (realMsgIds.contains(messageId)) {
-				client1SmppSessionHandler.redisUtil.lPush(SmppServerConstants.CM_DELIVER_SM, deliverSm);
-			} else {
-				client1SmppSessionHandler.redisUtil.lPush(SmppServerConstants.WEB_DELIVER_SM, deliverSm);
-			}
-
-		} catch (Exception e) {
-			logger.error("{}-处理短信状态报告内容解析异常", Thread.currentThread().getName(), e);
-			return;
-		}
-
-	}
+    @PostConstruct
+    public void init() {
+        client1SmppSessionHandler = this;
+        client1SmppSessionHandler.redisUtil = this.redisUtil;
+    }
 
 
-	@Override
-	public void fireExpectedPduResponseReceived(PduAsyncResponse pduAsyncResponse) {
-		super.fireExpectedPduResponseReceived(pduAsyncResponse);
-	}
+    @Override
+    public void setSmppSession(SmppSession session) {
+        this.sessionRef = new WeakReference<>(session);
+    }
 
-	@Override
-	public void fireUnexpectedPduResponseReceived(PduResponse pduResponse) {
-		super.fireUnexpectedPduResponseReceived(pduResponse);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(Client1SmppSessionHandler.class);
 
-	@Override
-	public void fireUnrecoverablePduException(UnrecoverablePduException e) {
-		super.fireUnrecoverablePduException(e);
-	}
+    public Client1SmppSessionHandler() {
+        super();
+    }
 
-	@Override
-	public void fireRecoverablePduException(RecoverablePduException e) {
-		super.fireRecoverablePduException(e);
-	}
+    public Client1SmppSessionHandler(Logger logger) {
+        super(logger);
+    }
 
-	@Override
-	public void fireUnknownThrowable(Throwable t) {
-		super.fireUnknownThrowable(t);
-	}
+    @Override
+    public String lookupResultMessage(int commandStatus) {
+        return super.lookupResultMessage(commandStatus);
+    }
 
-	@Override
-	public void firePduRequestExpired(PduRequest pduRequest) {
-		super.firePduRequestExpired(pduRequest);
-	}
+    @Override
+    public String lookupTlvTagName(short tag) {
+        return super.lookupTlvTagName(tag);
+    }
 
-	@Override
-	public boolean firePduReceived(Pdu pdu) {
-		return super.firePduReceived(pdu);
-	}
+    @Override
+    public void fireChannelUnexpectedlyClosed() {
+        super.fireChannelUnexpectedlyClosed();
+    }
 
-	@Override
-	public boolean firePduDispatch(Pdu pdu) {
-		return super.firePduDispatch(pdu);
-	}
+    @Override
+    public PduResponse firePduRequestReceived(PduRequest pduRequest) {
+        PduResponse response = pduRequest.createResponse();
+        SmppSession session = this.sessionRef.get();
+        String systemId = "";
+        if (session != null) {
+            systemId = session.getConfiguration().getSystemId();
+        }
+
+        try {
+            if (pduRequest.isRequest()) {
+                switch (pduRequest.getCommandId()) {
+                    case SmppConstants.CMD_ID_SUBMIT_SM:
+                        logger.error("----------客户端怎么会接到短信请求呢 ---------------");
+                        break;
+                    case SmppConstants.CMD_ID_DELIVER_SM:
+                        DeliverSm deliverSm = (DeliverSm) pduRequest;
+
+                        deliverSm.setSystemId(systemId);
+
+                        try {
+                            //0 je   1 redis
+                            if ("1".equals(StaticValue.TYPE)) {
+                                putRedisCache(deliverSm);
+                            } else {
+                                BlockingQueue<Object> queue = BDBStoredMapFactoryImpl.INS.getQueue("rptrvok", "rptrvok");
+                                queue.put(deliverSm);
+                            }
+
+                        } catch (Exception e) {
+                            logger.error("----------状态报告接收，加入队列异常。---------------", e);
+                        }
+
+                        break;
+                    case SmppConstants.CMD_ID_DATA_SM:
+                        return response;
+                    case SmppConstants.CMD_ID_ENQUIRE_LINK:
+                        logger.info("---------客户端{}也会接收心跳请求----------{}", systemId, pduRequest);
+                        return response;
+                    case SmppConstants.CMD_ID_UNBIND:
+                        logger.info("---------客户端{}被解绑了----------{}", systemId, pduRequest);
+                        return response;
+                    default:
+                        logger.error("----------客户端{}接收 未知异常。---------------{}", systemId, pduRequest);
+                        return response;
+
+                }
+            }
+
+        } catch (Exception e) {
+            logger.error("--------客户端处理异常---------", e);
+        }
+
+        // do any logic here
+
+        return response;
+    }
+
+    /**
+     * 将状态报告缓存到对应的redis中，交由不同的服务端处理
+     *
+     * @param deliverSm
+     */
+    public static void putRedisCache(DeliverSm deliverSm) {
+        try {
+//			Object obj = client1SmppSessionHandler.redisUtil.hmGetAllValues(SmppServerConstants.CM_MSGID_CACHE);
+//			List<String> realMsgIds = (List<String>) obj;
+
+            //获取状态报告msgid
+            String str = new String(deliverSm.getShortMessage());
+            DeliveryReceipt deliveryReceipt = DeliveryReceipt.parseShortMessage(str, DateTimeZone.UTC);
+
+            String messageId = deliveryReceipt.getMessageId();
+            Object obj = client1SmppSessionHandler.redisUtil.hmGet(SmppServerConstants.CM_MSGID_CACHE, messageId);
+
+            //判断msgid是否在cm的msgid缓存中存在
+            if (obj != null) {
+                client1SmppSessionHandler.redisUtil.lPush(SmppServerConstants.CM_DELIVER_SM, deliverSm);
+            } else {
+                client1SmppSessionHandler.redisUtil.lPush(SmppServerConstants.WEB_DELIVER_SM, deliverSm);
+            }
+
+        } catch (Exception e) {
+            logger.error("{}-处理短信状态报告内容解析异常", Thread.currentThread().getName(), e);
+            return;
+        }
+
+    }
+
+
+    @Override
+    public void fireExpectedPduResponseReceived(PduAsyncResponse pduAsyncResponse) {
+        super.fireExpectedPduResponseReceived(pduAsyncResponse);
+    }
+
+    @Override
+    public void fireUnexpectedPduResponseReceived(PduResponse pduResponse) {
+        super.fireUnexpectedPduResponseReceived(pduResponse);
+    }
+
+    @Override
+    public void fireUnrecoverablePduException(UnrecoverablePduException e) {
+        super.fireUnrecoverablePduException(e);
+    }
+
+    @Override
+    public void fireRecoverablePduException(RecoverablePduException e) {
+        super.fireRecoverablePduException(e);
+    }
+
+    @Override
+    public void fireUnknownThrowable(Throwable t) {
+        super.fireUnknownThrowable(t);
+    }
+
+    @Override
+    public void firePduRequestExpired(PduRequest pduRequest) {
+        super.firePduRequestExpired(pduRequest);
+    }
+
+    @Override
+    public boolean firePduReceived(Pdu pdu) {
+        return super.firePduReceived(pdu);
+    }
+
+    @Override
+    public boolean firePduDispatch(Pdu pdu) {
+        return super.firePduDispatch(pdu);
+    }
 }
